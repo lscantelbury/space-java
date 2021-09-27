1b44da20d34f95e7a9ee095fe62bc6202bb52f77
Luís <lhsda.snf20@uea.edu.br>
2021-09-27 12:29:55 -0400

    Browser Support

---
+++ spaceGame/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"spaceGame","l":"Board"},{"p":"spaceGame","l":"Bullet"},{"p":"spaceGame","l":"Enemy"},{"p":"spaceGame","l":"Main"},{"p":"spaceGame","l":"Player"}]

bcc0572331bcbff8aa81e5967a63fe38c9e343fa
Luís <lhsda.snf20@uea.edu.br>
2021-09-27 11:54:17 -0400

    User Interface

--- spaceGame/src/spaceGame/Main.java
+++ spaceGame/src/spaceGame/Main.java
@@ -7,6 +7,15 @@ public class Main {
 		Bullet bullet = new Bullet(spaceShip.getPosition(), 5, 1, 1, false);
 		Board board = new Board(spaceShip, bullet);
 		
+		System.out.println("SPACE IN JAVA! - A CLI GAME");
+		System.out.println();
+		System.out.println("*4 = SpaceShip");
+		System.out.println("*3 = Alien");
+		System.out.println("*1 = Laser");
+		System.out.println();
+		System.out.println("DON'T LET THEM GET TO YOU!");
+		System.out.println("--------------------------");
+		
 		do {
 			board.printBoard();
 		}while(spaceShip.getCondition() == "alive");
